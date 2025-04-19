949099b891924a3e31d83a011ab161db0ba707a1
kazu-321 <kzs321kzs@gmail.com>
2025-04-19 23:09:56 +0900

    add: canable

---
+++ canable/include/canable/canable_node.hpp
@@ -0,0 +1,7 @@
+#ifndef __CANABLE_NODE_HPP__
+#define __CANABLE_NODE_HPP__
+
+#include "rclcpp/rclcpp.hpp"
+#include "canable_msgs/msg/Can.hpp"
+
+#endif// __CANABLE_NODE_HPP__
---
+++ canable/src/canable_node.cpp
@@ -0,0 +1,1 @@
+#include "canable/canable_node.hpp"
